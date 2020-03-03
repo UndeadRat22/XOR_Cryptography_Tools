caaa9665e401b47eb65adc92d66f4bc518c81dfe
Valdas Rakutis <valdas@rakutis.lt>
2020-03-03 14:45:14 +0200

    xor encrypt/decrypt finished

--- xor_enc/src/main.rs
+++ xor_enc/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let text = &args[1];
+    let key = &args[2];
+
+    let encrypted = enc(text.to_string(), key.to_string());
+
+    println!("{:?}", encrypted);
+    
+    let decrypted = enc(encrypted, key.to_string());
+    
+    println!("{:?}", decrypted);
+}
+
+fn enc(_text: String, _key: String) -> String {
+    let key_bytes = _key.as_bytes();
+    let mut result_array = vec![0; _text.len()];
+    for (i, char_byte) in _text.as_bytes().iter().enumerate() {
+        result_array[i] = char_byte ^ &key_bytes[i % _key.len()];
+    }
+    return String::from_utf8(result_array).unwrap();
 }
